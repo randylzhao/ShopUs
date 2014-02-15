11a6a089065232b6e2eb47dfc1dc4fd74bafa825
Ronnie Ghose <ronnie.ghose@gmail.com>
2014-02-15 03:49:48 -0500

    merp

--- hello.py
+++ hello.py
@@ -3,7 +3,8 @@ from flask import Flask
 from flask.ext.pymongo import PyMongo
 
 app = Flask(__name__)
-app.config['MONGO_URI'] = 'mongodb://heroku_app22228003:nnk0noj15se1nk9bfjf5ofo165@ds027769.mongolab.com:27769/heroku_app22228003'
+if not hasattr(app.config, 'MONGO_URI'):
+	app.config['MONGO_URI'] = 'mongodb://heroku_app22228003:nnk0noj15se1nk9bfjf5ofo165@ds027769.mongolab.com:27769/heroku_app22228003'
 mongo = PyMongo(app)
 
 @app.route('/')
