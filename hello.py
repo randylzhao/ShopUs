fe6de00c4e263df326ff4606420a1d504352e612
root <root@Webtest.mydomain>
2014-02-15 14:59:02 -0500

    fixed bugs

--- hello.py
+++ hello.py
@@ -70,7 +70,7 @@ def adlogin():
         password = request.form['login_password']
         if not (username in session):
             if login(username, password):
-                session['user'] = name
+                session['user'] = username
                 return redirect(url_for('adhome'))
             else:
                 return render_template('login.html')
