3b22897e68d807b15ee906fbdce7d107641a238c
Ronnie Ghose <ronnie.ghose@gmail.com>
2014-02-15 06:12:20 -0500

    run the debugger

--- hello.py
+++ hello.py
@@ -3,6 +3,7 @@ from flask import Flask, url_for, render_template, request,flash
 from flask.ext.pymongo import PyMongo
 
 app = Flask(__name__)
+
 if not hasattr(app.config, 'MONGO_URI'):
 	app.config['MONGO_URI'] = 'mongodb://heroku_app22228003:nnk0noj15se1nk9bfjf5ofo165@ds027769.mongolab.com:27769/heroku_app22228003'
 mongo = PyMongo(app)
@@ -51,3 +52,5 @@ def upd():
     db = mongo.db
     db.posts.insert({"User": "Rex", "Role": "Test"})
     return 'Success'
+
+app.run(debug=True)
