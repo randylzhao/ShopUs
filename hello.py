5676b895cfb1ca1ffe4a41bb4fdf7080b4ff5492
Ronnie Ghose <ronnie.ghose@gmail.com>
2014-02-15 06:07:26 -0500

    add login hopefully

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, url_for, render_template, request,flash
 from flask.ext.pymongo import PyMongo
 
 app = Flask(__name__)
@@ -19,18 +19,28 @@ def retrieve():
 @app.route('/login', methods = ['GET', 'POST'])
 def advertiserhome():
     if request.method == 'POST':
-        username = request.method['username']
-        password = request.method['password']
+        username = request.method['login_email']
+        password = request.method['login_password']
         login(username, password)
     else:
         return render_template('login.html')
-    
+
+@app.route('/signup', methods = ['GET','POST'])
+def add_advertiser():
+	if request.method == 'POST':
+		username = request.method['signup_email']
+		password = request.method['signup_password']
+		if(db.users.find_one({"User":username}) != None):
+			flash("that username is already in use")
+			return render_template('signup.html')
+		else:
+			return render_template('signup.html')
+
 def login(name, password):
     db = mongo.db
     usr_obj = db.users.find_one({"User":name})
-    
-    if 
-        
+	if( (usb_obj != None) and (usb_obj['password']=password)):
+		return render_template('login_success.html')
 
 @app.route('/user')
 def user():
