a9bf55bed1054ebeee0c92359030f480bd1bf9ee
root <root@Webtest.mydomain>
2014-02-15 02:45:24 -0500

    fixes

--- hello.py
+++ hello.py
@@ -1,8 +1,18 @@
 import os
 from flask import Flask
+from flask.ext.pymongo import PyMongo
 
 app = Flask(__name__)
+app.config['MONGO_URI'] = 'mongodb://heroku_app22228003:nnk0noj15se1nk9bfjf5ofo165@ds027769.mongolab.com:27769/heroku_app22228003'
+mongo = PyMongo(app)
+
 
 @app.route('/')
 def hello():
-    return 'Hello World'
+    return mongo.db.find_one({"User":"Rex"})
+
+@app.insert('/insert')
+def upd():
+    db = mongo.db
+    db.posts.insert({"User": "Rex", "Role": "Test"})
+    return 'Success'
