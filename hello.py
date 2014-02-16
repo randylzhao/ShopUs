939c19ae4b6fb0dd3a1e4ca71750bf1cba5928bb
root <root@ubuntu-randy.(none)>
2014-02-16 00:47:01 -0500

    asdf

--- hello.py
+++ hello.py
@@ -153,6 +153,8 @@ def found_item():
 	else:
             #hunt is valid, so add a number to numbers database
 	    db.numbers.insert({"Number": number, "activehunt": active_hunt, "cluenumber": 0})
+	    part_num = active_hunt['participants'] + 1
+	    db.hunts.update({'_id': active_hunt['_id'], {'participants' : partnum})
 	    keys = active_hunt['keys']
 	    message = message + "You have registed for " + active_hunt['huntname'] + ". Find " + keys[0]
 	    resp = twilio.twiml.Response()
